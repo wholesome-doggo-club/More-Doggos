65a329043d7642ed8a66fdfd3be43d42000d848f
dustintktran <dustintktran@gmail.com>
2019-05-15 15:21:07 -0700

    basic doggo creation

--- client/src/components/DogLink.jsx
+++ client/src/components/DogLink.jsx
@@ -4,21 +4,66 @@ class DogCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hovering: false,
+      shareHover: false,
+      shareClick: false
     }
-
+    this.handleShareHover = this.handleShareHover.bind(this);
+    this.handleShareClick = this.handleShareClick.bind(this);
   }
 
-  render() {
+  handleShareHover() {
+    if (this.state.shareHover === false) {
+      this.setState({ shareHover: true });
+    } else {
+      this.setState({ shareHover: false });
+    }
+  }
 
+  handleShareClick() {
+    if (this.state.shareClick === false) {
+      this.setState({ shareClick: true })
+    } else {
+      this.setState({ shareClick: false })
+    }
+  }
+
+
+  render() {
     return (
       <div>
         <div className="dogcard-outer">
-          <div className="dogcard-top">
-            <img src="https://plannthat.com/wp-content/uploads/2017/06/DSC_0438.jpg" className="dogcard-image"></img>
-            <div className="dogcard-filler"></div>
+          <div>
+            <div className="dogcard-top">
+              <img src="https://plannthat.com/wp-content/uploads/2017/06/DSC_0438.jpg" className="dogcard-image"></img>
+              <div className="dogcard-filler"></div>
+            </div>
+            <div className="dogcard-name">BILLY</div>
           </div>
-          <div className="dogcard-name">Billy</div>
+          <div className="dogcard-hover">
+            <div>
+              <img src="https://plannthat.com/wp-content/uploads/2017/06/DSC_0438.jpg" className="dogcard-image-round"></img>
+              {!this.state.shareHover &&
+                <img src="./files/shareDefault.jpg" className="dogcard-share" onMouseEnter={this.handleShareHover}></img>}
+
+              {this.state.shareHover &&
+                <img src="./files/shareHover.jpg" className="dogcard-share" onMouseLeave={this.handleShareHover} onClick={this.handleShareClick}></img>}
+            </div>
+            {!this.state.shareClick &&
+              <div className="dogcard-info">
+                <div className="dogcard-info-name">Billy</div>
+                <div className="dogcard-info-extra">Doggo Terrier</div>
+                <div>Young | Male</div>
+                <div>Los Angelos, CA</div>
+              </div>
+            }
+            {this.state.shareClick &&
+              <div className="dogcard-info2">
+                <div className="dogcard-info-share">Share Billy</div>
+                <div className="dogcard-share-buttons">Ya -- Ba -- Da -- doop</div>
+              </div>
+            }
+          </div>
+          
         </div>
       </div>
     )
